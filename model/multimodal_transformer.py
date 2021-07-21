ff99ba53cd01c8305f809b0eb159ffa66de18c45
Antoine Yang <49347253+antoyang@users.noreply.github.com>
2021-07-21 17:20:37 +0200

    Add files via upload

--- model/multimodal_transformer.py
+++ model/multimodal_transformer.py
@@ -10,14 +10,15 @@ from transformers import DistilBertConfig
 
 
 def create_sinusoidal_embeddings(n_pos, dim, out):
-    position_enc = np.array(
-        [
-            [pos / np.power(10000, 2 * (j // 2) / dim) for j in range(dim)]
-            for pos in range(n_pos)
-        ]
-    )
-    out[:, 0::2] = torch.FloatTensor(np.sin(position_enc[:, 0::2]))
-    out[:, 1::2] = torch.FloatTensor(np.cos(position_enc[:, 1::2]))
+    with torch.no_grad():
+        position_enc = np.array(
+            [
+                [pos / np.power(10000, 2 * (j // 2) / dim) for j in range(dim)]
+                for pos in range(n_pos)
+            ]
+        )
+        out[:, 0::2] = torch.FloatTensor(np.sin(position_enc[:, 0::2]))
+        out[:, 1::2] = torch.FloatTensor(np.cos(position_enc[:, 1::2]))
     out.detach_()
     out.requires_grad = False
 
